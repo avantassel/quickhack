f705942b87a6de2e800a023c3238ab8383f44967
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-01-30 18:37:18 -0700

    Added timer and FB user login

---
+++ README.md
@@ -0,0 +1,7 @@
+# Brew Timers #
+
+Was create for a QuickLeft brewfest hackfest.
+
+```sh
+python -m SimpleHTTPServer
+```
--- js/main.js
+++ js/main.js
@@ -1,30 +1,115 @@
 var app = angular.module("brewapp", ["firebase"]);
+var FBURL = "https://quickhack.firebaseio.com";
 
-function SidebarController($scope, $firebase) {
-	var timersRef = new Firebase("https://quickhack.firebaseio.com/timers");
-	// Automatically syncs everywhere in realtime
-	$scope.timers = $firebase(timersRef);
+function SidebarCtrl($scope, $firebase) {
+	
+	$scope.timers = $firebase(new Firebase(FBURL+'/timers'));
+
 }
 
-function MainController($scope, $firebase) {
-	var timersRef = new Firebase("https://quickhack.firebaseio.com/timers");
-	// Automatically syncs everywhere in realtime
-	$scope.timers = $firebase(timersRef);
+function MainCtrl($scope, $firebase, $interval) {
+	
+	$scope.timers = $firebase(new Firebase(FBURL+'/users/0/timers'));
 
-	$scope.addTimer = function() {
-		$hsl = Math.random() * (360 - 1) + 1;
-	    $scope.timers.$add({name: $scope.name, time: '3600', hsl: Math.floor($hsl) });
+	$scope.auth = new FirebaseSimpleLogin(new Firebase(FBURL), function(error, user) {
+		  if (error) {
+		    // an error occurred while attempting login
+		    console.log(error);
+		  } else if (user) {
+		    // user authenticated with Firebase
+		    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+		    //create new user
+		    $exists = new Firebase(FBURL).child('users/'+user.id);
+		    if(!$exists)
+		    	new Firebase(FBURL).child('users/'+user.id).set(user);
+		    $scope.user=user;
+		    $scope.loadUserTimers();
+
+		  } else {
+		    console.log('logged out');
+		  }
+		});
+
+	$scope.setUserPhoto = function(){
+		if($scope.user.id)
+			return 'http://graph.facebook.com/'+$scope.user.id+'/picture';	
+		else
+			return '';
+	}
+	
+
+	$scope.logout = function(){
+
+		$scope.user = "";
+		$scope.timers = {};
+		$scope.auth.logout();
+
 	}
 
-	$scope.timerRunning = true;
+	$scope.login = function(){
+		
+		$scope.auth.login('facebook', {
+		  rememberMe: true,
+		  scope: 'email,user_likes'
+		});
+	}
 
-    $scope.startTimer = function (){
-        $scope.$broadcast('timer-start');
-        $scope.timerRunning = true;
+	$scope.loadUserTimers = function(){
+
+		$scope.timers = $firebase(new Firebase(FBURL+'/users/'+$scope.user.id+'/timers'));
+
+	}
+
+	$scope.addTimer = function(elem) {
+		$hsl = Math.random() * (360 - 1) + 1;
+		
+		if($scope.user)
+			$scope.timers.$add({name: $scope.timerName, duration: 3600, elapsed:0, hsl: Math.floor($hsl), userId: $scope.user.id, userName: $scope.user.displayName});
+		else
+			$scope.timers.$add({name: $scope.timerName, duration: 3600, elapsed:0, hsl: Math.floor($hsl), userId: '0', userName: 'Anonymous'});
+	}
+
+	$scope.startTimer = function (timer,elem){
+
+		var newtimer = $interval(function () {}, 1000);
+
+		$scope.timers.elapsed++;
+
+    	function success() {
+        	console.log("done");
+	    }
+
+	    function error() {
+	        console.log("cancelled or error");
+	    }
+
+	    function notify() {
+	        console.log("updating");
+	    }
+
+	    newtimer.then(success, error, notify)
+
     };
 
-    $scope.stopTimer = function (){
-        $scope.$broadcast('timer-stop');
-        $scope.timerRunning = false;
+    $scope.stopTimer = function (e){
+        
+    };
+
+    $scope.addMin = function (e){
+        
+    };
+
+    $scope.subMin = function (e){
+        
+    };
+
+    $scope.getMin = function (timer){
+    	var time = timer.duration-timer.elapsed;
+        return Math.floor(time / 60);
+    };
+
+    $scope.getSec = function (timer){
+    	var time = timer.duration-timer.elapsed;
+        return time - $scope.getMin(timer) * 60;
     };
 }
